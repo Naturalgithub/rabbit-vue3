8c48b93a9ae5e51d9035f91143a1567e54f600bf
james <3318615021@qq.com>
2022-03-15 10:26:50 +0800

    项目初始化

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,22 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  <h1>hello</h1>
+  <router-view></router-view>
 </template>
 
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+<script>
+import axios from '@/utils/request'
+export default {
+  async created () {
+    const res = await axios.get('/home/index')
+    console.log(res)
   }
+}
+</script>
+<style lang="less" scoped>
+@import '~@/assets/styles/variables.less';
+@import '~@/assets/styles/mixins.less';
+h1 {
+  background-color: @xtxColor;
+  .hoverShadow();
 }
 </style>
